64c63665646c9bc285c39f15a169b463d74098ce
Antonio Peña Batista <apenab1995@gmail.com>
2022-04-10 23:19:44 +0200

    feat: added feature to persist query state in the local storage

--- src/query/config.js
+++ src/query/config.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { QueryClient } from "react-query";
+import { persistQueryClient } from "react-query/persistQueryClient-experimental";
+import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental";
 
 export const API_URL = "https://fakestoreapi.com/";
 
@@ -10,7 +12,19 @@ export const queryClient = new QueryClient({
         const { data } = await axios.get(`${API_URL}${queryKey[0]}`);
         return data;
       },
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
       refetchOnWindowFocus: false,
     },
   },
+});
+
+const localStoragePersistor = createWebStoragePersistor({
+  storage: window.localStorage,
+});
+
+// persistQueryClient is a utility for persisting the state of the queryClient and its caches for later use
+// https://react-query.tanstack.com/plugins/persistQueryClient
+persistQueryClient({
+  queryClient,
+  persistor: localStoragePersistor,
 });
